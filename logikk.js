8a31ea71cc5dd8726b39dccde09fcf1f8ac19109
banyalvansh <122355156+banyalvansh@users.noreply.github.com>
2023-03-15 10:16:47 +0530

    initial commit

---
+++ logikk.js
@@ -0,0 +1,22 @@
+let noButton = document.getElementById("no");
+
+noButton.addEventListener('mouseover', e => {
+    let x = Math.floor(Math.random() * (screen.availWidth - 200))
+    let y = Math.floor(Math.random() * (screen.availHeight - 200))
+    console.log('x '+ x + ', y ' + y);
+    noButton.style.position = "absolute"
+    noButton.style.left = x + 'px'
+    noButton.style.top = y +'px'
+})
+
+
+let yesButton = document.getElementById("yes");
+
+yesButton.addEventListener('click', e => {
+    console.log(document.getElementById('ques'));
+    document.getElementById('ques').innerHTML = "Love You Too😘😍❤️";
+    yesButton.style.display="none";
+    noButton.style.display="none";
+
+})
+ 
